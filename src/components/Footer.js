0bb4b73434d65f7fbf07672931fa94f1a59b1d7f
KurmaSweety <86476896+KurmaSweety@users.noreply.github.com>
2024-02-01 13:09:03 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,18 @@
 import React from 'react'
-import { pageLinks,socialLinks } from '../data'
+import { socialLinks } from '../data'
 import PageLinks from './PageLinks';
 import SocialLinks from './SocialLinks';
-function Footer() {
+const Footer=()=> {
   return (
     <footer className="section footer">
       <PageLinks parentClass='footer-links' itemClass='footer-link'/>
-      <ul className="footer-links">
-        {pageLinks.map((link)=>{
-          const {id,href,text}=link;
-          return(
-            <SocialLinks key={link.id} {...link} itemClass="footer-link"/>
-             
-          )
-        })}
-      </ul>
       <ul className="footer-icons">
         {socialLinks.map((link)=>{
-          const {id,href,icon}=link;
-          return(
-            <li key={id}>
-            <a href={href} target="_blank" className="footer-icon">
-              <i className={icon}></i></a>
-          </li>
-          )
+          return
+            <SocialLinks key={link.id} {...link} itemClass="footer-icon"/>
         })}
       </ul>
+       
       <p className="copyright">
         copyright &copy; Backroads travel tours company
         <span id="date">{new Date().getFullYear()}</span> all rights reserved
